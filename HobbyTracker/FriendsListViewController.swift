b86cb1ff57e3eb43f8b63430a364a3763818398a
Joshua Franklin <jfranklin1607@gmail.com>
2019-08-13 16:28:24 -0400

    hobby-tracker

---
+++ HobbyTracker/FriendsListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FriendsListViewController.swift
+//  HobbyTracker
+//
+//  Created by Joshua Franklin on 8/13/19.
+//  Copyright © 2019 AMSU. All rights reserved.
+//
+
+import UIKit
+
+class FriendsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
